de98f38ae7ac03dde4fa5462848af57f9f532af5
nicole <njl55@cornell.edu>
2025-11-12 14:25:04 -0500

    added data preprocess and vector_db

--- data_preprocess.py
+++ data_preprocess.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2fb3cbe739548a02f771cdc7ac09190249546df8e99e6f290c1378fa705651e0
-size 3284
+oid sha256:4bcf250958ff7f773b543237dde3b5c04a1cd4bde98037ecea4ff83e59b4abbe
+size 3285
--- vector_db.py
+++ vector_db.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:08d4fa4938866209d78b9def8c28522de57ae3a8f1f47ee2d89dc39e7f68b841
-size 3365
+oid sha256:33c1dadf2f597f5cfbcaba11ccf35f9c5556e25b3de25e57d1517df606cf27e1
+size 3366
